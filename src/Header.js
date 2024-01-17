546facc2e63bb8f75e98ff182eaaa08b98f795a1
DharmikVaja <vajadharmikk@gmail.com>
2024-01-17 17:28:19 +0530

    Add Header component and use it in Index

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { ReactDOM } from "react-dom";
+
+function Header() {
+  return (
+    (
+      <>
+        <h1>React App Header</h1>
+      </>
+    ),
+    document.getElementById("root")
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import User from "./User";
+import Header from "./Header";
 // import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/300/300";
 const img3 = "https://picsum.photos/250/300";
-const myName = "Dharmik";
 
 let currDate = new Date().getHours();
 let greeting = "";
 if (currDate >= 1 && currDate <= 12) {
   greeting = "Good Morning";
-} else if (currDate >= 12 && currDate <= 19) {
+} else if (currDate >= 12 && currDate <= 16) {
   greeting = "Good Afternoon";
+} else if (currDate >= 16 && currDate <= 20) {
+  greeting = "Good Evening";
 } else {
   greeting = "Good Night";
 }
 
 root.render(
   <React.StrictMode>
-    <h1>
-      Hello,{greeting}!! my name is {myName}; and This is Images:
-    </h1>
+    <h1>Hello,{greeting}!! and This is Images:</h1>
     <div className="images">
       <img src={img1} alt="random images" />
       <img src={img2} alt="random images" />
@@ -33,7 +33,15 @@ root.render(
         <img src={img3} alt="random images" className="img3" />
       </a>
     </div>
+    <ul>
+      <li>This</li>
+      <li>is</li>
+      <li>a</li>
+      <li>List</li>
+    </ul>
+
     <App />
+    <User />
   </React.StrictMode>
 );
 
